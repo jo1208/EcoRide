d3192b86e8cdb80c1e9b2f765f5e38c59bc9a2e9
unknown <jonathan.pina1208@gmail.com>
2025-05-08 15:22:08 +0200

    Fix: escape 'user' table name for PostgreSQL

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -14,7 +14,7 @@ use App\Entity\Preference;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\Table(name: "`user`")]
+#[ORM\Table(name: '"user"')]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
 #[UniqueEntity(fields: ['email'], message: 'Cet email est déjà utilisé.')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
