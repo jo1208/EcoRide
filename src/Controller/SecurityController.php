2b323cc173c800c701d84f4bbf20fcd60c65cb59
unknown <jonathan.pina1208@gmail.com>
2025-05-09 13:42:36 +0200

    modif

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,41 +4,24 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
+    /**
+     * @Route("/login", name="app_login")
+     */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // Si l'utilisateur est déjà connecté, on le redirige ailleurs
-        if ($this->getUser()) {
-            return $this->redirectToRoute('some_success_route');
-        }
-
+        // Obtient les erreurs de connexion et le dernier nom d'utilisateur
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
+        // Rendu de la page de connexion avec les erreurs et le dernier nom d'utilisateur
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
-    }
-
-
-    #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): void
-    {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-    }
-
-    #[Route('/debug-csrf', name: 'debug_csrf', methods: ['GET'])]
-    public function debugCsrf(CsrfTokenManagerInterface $csrfTokenManager): Response
-    {
-        $token = $csrfTokenManager->getToken('authenticate')->getValue();
-        return new Response('CSRF Token généré : ' . $token);
     }
 }
