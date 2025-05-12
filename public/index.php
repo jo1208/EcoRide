69d133509cc734e517f1ea7a6bb3a1573743a78d
unknown <jonathan.pina1208@gmail.com>
2025-05-12 16:47:20 +0200

    Ajout trusted proxy pour corriger CSRF sur Heroku

--- public/index.php
+++ public/index.php
@@ -1,9 +1,18 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+// ✅ Ajout pour corriger le CSRF sur Heroku (proxy trust)
+Request::setTrustedProxies(
+    ['127.0.0.1', '::1'],
+    Request::HEADER_X_FORWARDED_ALL
+);
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
