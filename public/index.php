4fc9f65408fbe68b36199241c02413780e126601
unknown <jonathan.pina1208@gmail.com>
2025-05-12 16:54:00 +0200

    index

--- public/index.php
+++ public/index.php
@@ -1,18 +1,9 @@
 <?php
 
 use App\Kernel;
-use Symfony\Component\HttpFoundation\Request;
 
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
-// ✅ Corrige la gestion des proxys pour Heroku avec Symfony 7
-Request::setTrustedProxies(
-    [$_SERVER['REMOTE_ADDR']],
-    Request::HEADER_FORWARDED
-);
-
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
